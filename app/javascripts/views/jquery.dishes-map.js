27bd6428c0c71041ec06ff94f0ffda9f9a2623dc
Iva Popova <iva.svetoslavova@gmail.com>
2013-11-03 16:35:32 +0100

    Added toggle and style of the options buttons

--- app/javascripts/views/jquery.dishes-map.js
+++ app/javascripts/views/jquery.dishes-map.js
@@ -1,3 +1,19 @@
+var mode_toggle = function(el){
+    if(el.name=="mode_d")
+    {
+        $('#btn_walk').removeAttr('disabled');
+        window.travelMode='driving';
+        el.disabled = 'disabled';
+    }
+    else if(el.name=="mode_w")
+    {
+        $('#btn_drive').removeAttr('disabled');
+        window.travelMode='walking';
+        el.disabled = 'disabled';
+    }
+    $('#map').patatmap('requestLocation');
+};
+
 $('#map').patatmap({
 
 });
