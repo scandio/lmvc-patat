4bfa2703010a8206759ebd6aadd202b45e8ff44a
Iva Popova <iva.svetoslavova@gmail.com>
2013-11-05 23:12:14 +0100

    Removed onclick from the map.html and moved it to the JS-code instead

--- app/javascripts/views/jquery.dishes-map.js
+++ app/javascripts/views/jquery.dishes-map.js
@@ -1,19 +1,27 @@
-var mode_toggle = function(el){
-    if(el.name=="mode_d")
+var mode_toggle = function(mode){
+    if(mode=="mode_d")
     {
         $('#btn_walk').removeAttr('disabled');
         window.travelMode='driving';
-        el.disabled = 'disabled';
+        $('#btn_drive').attr('disabled','disabled');
     }
-    else if(el.name=="mode_w")
+    else if(mode=="mode_w")
     {
         $('#btn_drive').removeAttr('disabled');
         window.travelMode='walking';
-        el.disabled = 'disabled';
+        $('#btn_walk').attr('disabled','disabled');
     }
     $('#map').patatmap('requestLocation');
 };
 
 $('#map').patatmap({
 
+});
+$('#btn_walk').on('click', function(e){
+    e.preventDefault();
+    mode_toggle('mode_w');
+});
+$('#btn_drive').on('click', function(e){
+    e.preventDefault();
+    mode_toggle('mode_d');
 });
