4e35ffd05bfea672d516a53efda7dd40909a1b8b
Tobias Deekens <nerd@tdeekens.name>
2013-11-09 10:37:03 +0100

    Adds sample configuration for Logger.

--- app/controllers/Application.php
+++ app/controllers/Application.php
@@ -3,12 +3,14 @@
 namespace controllers;
 
 use Scandio\lmvc\modules\security\AnonymousController;
+use Scandio\lmvc\utils\logger\Logger;
 use Scandio\lmvc\LVC;
 
 class Application extends AnonymousController
 {
     public static function index()
     {
+        Logger::instance()->info('The Patat {what}!', ['what' => 'started']);
         return static::render();
     }
 }
