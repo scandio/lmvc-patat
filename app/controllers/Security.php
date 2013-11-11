4010f1240be15600be607abe8ef21a04515c9a5a
anna-averianova <ann.averyanova@gmail.com>
2013-11-11 10:45:58 +0100

    Adds Security controller to check if email has been verified during authentication

--- app/controllers/Security.php
+++ app/controllers/Security.php
@@ -12,5 +12,30 @@ use Scandio\lmvc\utils\config\Config;
 
 class Security extends controllers\Security
 {
+    /**
+     * Overrides lmvc's authentication function to include
+     * check for email verification
+     * @return array
+     */
+    protected static function authenticate()
+    {
+        #Call the parent function first
+        $parentResponse = parent::authenticate();
 
+        if ($parentResponse['success'] === true) {
+            $user = \models\Users::findBy('username', static::request()->username) -> one();
+
+            if ($user->verified == 1){
+                return $parentResponse;
+            } else {
+                return [
+                    'success' => false,
+                    'controllerAction' => 'Security::login',
+                    'params' => ['failure']
+                ];
+            }
+        } else {
+            return $parentResponse;
+        }
+    }
 }
