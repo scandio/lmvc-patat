02aff50e62e4e6fd94baf0aab7d305d0bd6f695d
Tobias Deekens <nerd@tdeekens.name>
2013-11-12 10:32:03 +0100

    Changes Session::set to be performed by Patat's Security controller.

--- app/controllers/Security.php
+++ app/controllers/Security.php
@@ -4,6 +4,7 @@ namespace controllers;
 
 use Scandio\lmvc;
 use Scandio\lmvc\Controller;
+use Scandio\lmvc\modules\session\Session;
 use Scandio\lmvc\modules\security\controllers;
 use \models;
 use \forms;
@@ -17,15 +18,18 @@ class Security extends controllers\Security
      * check for email verification
      * @return array
      */
-    protected static function authenticate()
+    protected static function authenticate($writeSession = true)
     {
         #Call the parent function first
-        $parentResponse = parent::authenticate();
+        $parentResponse = parent::authenticate(false);
 
         if ($parentResponse['success'] === true) {
             $user = \models\Users::findBy('username', static::request()->username) -> one();
 
             if ($user->verified == 1){
+                Session::set('security.current_user', static::request()->username);
+                Session::set('security.authenticated', true);
+
                 return $parentResponse;
             } else {
                 return [
